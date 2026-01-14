98a6580daa5d3ef7161bc8985142efbbbe4ee295
Tega Umukoro <your-email@example.com>
2026-01-14 15:16:42 -0600

    Fix index.tsx exports - remove deleted module references

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,5 @@
-
 export * from "./hero";
-export * from "./layout";
-export * from "./page";
-export * from "./our-stats";
-export * from "./about-event";
 export * from "./faq";
-export * from "./sponsored-by";
-export * from "./event-content";
-
-
+export * from "./clients";
+export * from "./features";
+export * from "./testimonials";
